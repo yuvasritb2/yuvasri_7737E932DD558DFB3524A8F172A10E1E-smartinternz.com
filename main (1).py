4765b347ca3ef5e6efc259b1890ff0461ddcd87d
yuvasritb2 <144519309+yuvasritb2@users.noreply.github.com>
2023-09-27 17:25:30 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,10 +1,17 @@
-year = int(input("Enter a year: "))
+class Player:
+  def play(self):
+    print ("The player is playing cricket ")
 
-if year % 400 == 0:
-    print(f"{year} is a leap year")
-elif year % 100 == 0:
-    print(f"{year} is not a leap year")
-elif year % 4 == 0:
-    print(f"{year} is a leap year")
-else:
-    print(f"{year} is not a leap year")
+class Batsman(Player):
+  def play(self):
+    print("The batsman is batting ")
+
+class Bowler(Player):
+  def play(self):
+    print("The bowler is bowling ")
+
+batsman=Batsman()
+bowler=Bowler()
+
+batsman.play()
+bowler.play()
--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
+class BankAccount:
+  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
+    self.__account_number = account_number
+    self.__account_holder_name = account_holder_name
+    self.__account_balance = initial_balance
 
-n=int(input("Enter the integer number"))
-result = factorial(n)
-print(f"The factorial of {n} is {result}")
+  def deposit(self, amount):
+    if amount > 0:
+      self.__account_balance += amount
+      print("Deposited ₹{}. New balance: ₹{}". format(amount, self.__account_balance))
+    else:
+      print("Invalid deposit amount.")
+
+  def withdraw(self, amount):
+    if amount > 0 and amount <= self.__account_balance:
+      self.__account_balance -= amount
+      print("Withdraw ₹{}. New balance: ₹{}". format(amount, self.__account_balance))
+    else:
+      print("Invalid withdrawal amount or insufficient balance.")
+
+  def display_balance(self):
+    print("Account balance for {} (Account #{}): ₹{}". format(self.__account_holder_name, self.__account_number, self.__account_balance))
+
+account = BankAccount(account_number = "4098840", account_holder_name = "Harini", initial_balance = 10000.0)
+
+account.display_balance()
+account.deposit(1000)
+account.withdraw(2000.0)
+account.display_balance()
